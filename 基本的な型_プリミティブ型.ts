13f385a1508b05824e4781728fbf1c0dea86ca4f
usxc <tinypopopo@gmail.com>
2025-09-20 20:07:59 +0900

    基本的な型のプリミティブ型

---
+++ 基本的な型_プリミティブ型.ts
@@ -0,0 +1,15 @@
+const isReady: boolean = false;
+const age: number = 25;
+const fullName: string = "John Doe";
+const bigNumber: bigint = 100n;
+const uniqueSymbol: symbol = Symbol("unique");
+const notDefined: undefined = undefined;
+const empty: null = null;
+
+// boolean: 真偽値。
+// number: 数値。
+// string: 文字列。
+// bigint: 大きな整数。
+// symbol: 一意の値を示す。
+// undefined: 値が定義されていない状態を示す。
+// null: 値が存在しない状態を示す。
