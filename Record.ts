eee547b7d96be74d2967643f0ef3b802ec29107f
usxc <tinypopopo@gmail.com>
2026-01-10 00:10:54 +0900

    Record

---
+++ Record.ts
@@ -0,0 +1,9 @@
+type ThreeLetterRecord = Record<"one" | "two" | "three", string>;
+
+// type ThreeLetterRecord = {
+//     one: string;
+//     two: string;
+//     three: string;
+// }
+
+// Recordは、オブジェクトのすべてのプロパティ値を特定の型に設定するユーティリティ型。
