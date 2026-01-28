c8b50ff4dd918f9848a7aa1dac2c1b4e0e6e871f
usxc <tinypopopo@gmail.com>
2026-01-28 21:45:53 +0900

    Map<k,v>

---
+++ Map.ts
@@ -0,0 +1,6 @@
+const map = new Map<string, number>();
+map.set("a", 1);
+console.log(map.get("a"));
+
+// Mapオブジェクトの作り方
+// Mapオブジェクトを作るにはMapクラスをnewします。たとえば、キーがstring、値がnumberのMap<string, number>は次のように作ります。
